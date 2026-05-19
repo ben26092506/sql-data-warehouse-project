556a37dfc6519f48e83e5aaf0765ed6b46e689dc
ben26092506 <benjamin.melinc@hotmail.com>
2026-05-19 13:33:09 +0200

    Update file paths for BULK INSERT statements

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -42,7 +42,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\Benjamin\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\Users\Benjamin\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -58,7 +58,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\Benjamin\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\Users\Benjamin\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -74,7 +74,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\Benjamin\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\Users\Benjamin\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -94,7 +94,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\Benjamin\Downloads\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		FROM 'C:\Users\Benjamin\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -110,7 +110,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\Benjamin\Downloads\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		FROM 'C:\Users\Benjamin\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -126,7 +126,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\Benjamin\Downloads\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'C:\Users\Benjamin\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
